3c1651ff507ab50c9652b919fb7be60be501c7d5
SamLondond <133343955+SamLondond@users.noreply.github.com>
2023-10-22 01:22:38 +0800

    Add files via upload

---
+++ RecordController.java
@@ -0,0 +1,49 @@
+package entity;
+
+public class RecordController {
+    String id ;
+    String sp ;
+    String op ; String content ;
+
+    @Override
+    public String toString() {
+        return "Record{" +
+                "id='" + id + '\'' +
+                ", sp='" + sp + '\'' +
+                ", op='" + op + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getSp() {
+        return sp;
+    }
+
+    public void setSp(String sp) {
+        this.sp = sp;
+    }
+
+    public String getOp() {
+        return op;
+    }
+
+    public void setOp(String op) {
+        this.op = op;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
